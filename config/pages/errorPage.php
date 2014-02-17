829e6d7d01dee9ae16333a93343efbb6045eacea
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-17 20:57:01 +0900

    remove old pane file

--- config/pages/errorPage.php
+++ config/pages/errorPage.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 //ページではテンプレートはレイアウトスクリプトである。内部にコンテナを実装するかどうか。複雑なページでは役に立つだろう。
 return array(
     'name' => 'error',
     'properties' => array(
         'title' => 'エラー',
-        'pane' => include __DIR__ . '/pane/draft1.php',
+        'pane' => 'base/top',
     ),
     'options' => array(
         'blockBuilder' => function ($b) {
