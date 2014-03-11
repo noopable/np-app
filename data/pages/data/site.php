42bb1146a58cac0fac25b5d7e903fe8fe31d61d3
Tomoaki Kosugi <kipspro@gmail.com>
2014-03-11 19:41:42 +0900

    Fix typo powered

--- data/pages/data/site.php
+++ data/pages/data/site.php
@@ -10,6 +10,6 @@
 return array(
     'site_label' => "AngularJS on \nGumbyFramework",
     'title' => 'Zend Framework2, AngularJS, GumbyFramework Demo',
-    'powerdBy' => 'Powerd by ZendFramework 2, AngularJS, Gumbyframework',
+    'poweredBy' => 'Powered by ZendFramework 2, AngularJS, Gumbyframework',
     'copyright' => 'copyright 2013-' . date('Y') . ' Kips Production Co.,Ltd',
 );
