c5249530879936cee68f8894c1846fab81daab54
Tomoaki Kosugi <kipspro@gmail.com>
2015-02-08 10:31:15 +0900

    Update Exception

--- src/NpApp/Exception/ExceptionInterface.php
+++ src/NpApp/Exception/ExceptionInterface.php
@@ -1,6 +1,13 @@
 <?php
+
+/**
+ *
+ *
+ * @copyright Copyright (c) 2013-2015 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
 namespace NpApp\Exception;
 
-interface ExceptionInterface {
-}
-
+interface ExceptionInterface
+{}
--- src/NpApp/Exception/RuntimeException.php
+++ src/NpApp/Exception/RuntimeException.php
@@ -1,9 +1,17 @@
 <?php
+/**
+ *
+ *
+ * @copyright Copyright (c) 2013-2015 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
 namespace NpApp\Exception;
+
 /**
  * Description of RuntimeException
  *
  * @author tomoaki
  */
-class RuntimeException extends \RuntimeException implements ExceptionInterface{
-}
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{}
