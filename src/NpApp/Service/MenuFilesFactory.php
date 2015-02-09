407fe9776c55dffa64af509bd95fe0d2102878ff
Tomoaki Kosugi <kipspro@gmail.com>
2015-02-09 19:46:58 +0900

    Update ServiceConfigs

--- src/NpApp/Service/MenuFilesFactory.php
+++ src/NpApp/Service/MenuFilesFactory.php
@@ -1,12 +1,15 @@
 <?php
-namespace NpApp\Service;
 /*
  *
  *
  * @copyright Copyright (c) 2013-2013 KipsProduction (http://www.kips.gr.jp)
  * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
  */
+
+namespace NpApp\Service;
+
 use Flower\File\Service\FileServiceFactoryFromConfig;
+
 /**
  * Description of MenuFilesFactory
  *
--- src/NpApp/Service/RepositoryPluginManager.php
+++ src/NpApp/Service/RepositoryPluginManager.php
@@ -1,30 +1,27 @@
 <?php
+/**
+ *
+ *
+ * @copyright Copyright (c) 2013-2015 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
 namespace NpApp\Service;
 
+use Flower\Model\Service\RepositoryPluginManager as AbstractRPM;
+
 /**
- * Description of RepositoryPluginManager
  *
  * @author tomoaki
  */
-class RepositoryPluginManager extends \Flower\Model\Service\RepositoryPluginManager {
-    
+class RepositoryPluginManager extends AbstractRPM
+{
     /**
      * クラスを配置する namespace as prefix
      * 他の場所のクラスを使いたいときは、直接getで取得するか、
      * 同じnamespaceにプロキシを配置する。
-     * 
-     * @var string 
+     *
+     * @var string
      */
     protected $pluginNameSpace = 'NpApp\Model\Repository';
-    
-    public function loadRepository($name, array $params = array())
-    {
-        //$repository = $this->getServiceLocator()->get('Di')->get($name, $params);
-
-        //$repository->initialize($this);
-        //initializeはinitializerが処理してくれる。
-        return $this->byName($name, $params);
-        //return $repository;
-    }
-
 }
